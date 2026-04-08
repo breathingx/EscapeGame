837e7799ccf5e2568457ce6d7d4821468a3024ec
fshadaw <filipheerkens@gmail.com>
2026-04-08 11:29:41 +0200

    cleanup of unused prototype dir

--- prototype/app/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-app.secret_key = "super_secret_key_here"  # TODO kies een betere key, een key is nodig om session te gebruiken
-from app import routes
