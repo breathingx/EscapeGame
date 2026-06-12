26a7df4fdff967b8e912aa3e7151eeae037311cd
GeneralDolf <93264568+GeneralDolf@users.noreply.github.com>
2026-06-12 15:09:17 +0200

    view.gs comments added

--- perfect-pitch/src/view.js
+++ perfect-pitch/src/view.js
@@ -21,13 +21,13 @@ export class View {
   constructor(gl, program, state) {
     this.gl = gl;
     this.program = program;
-
+    //Locate shader attributes
     this.aPosition = gl.getAttribLocation(program, 'aPosition');
     this.uMvp = gl.getUniformLocation(program, 'uMvp');
     this.uColor = gl.getUniformLocation(program, 'uColor');
-
+    //HUD Message element
     this.message = document.getElementById('message');
-
+    //Platform
     this.platform = new Cuboid(
       gl,
       0,
@@ -38,6 +38,7 @@ export class View {
       PLATFORM_DEPTH
     );
     this.sphere = new Sphere(gl);
+    //Gate mesh for every wall
     this.gates = state.gates.map((gate) => new Gate(gl, gate));
 
     this.gl.clearColor(...COLOR_BACKGROUND);
@@ -47,19 +48,21 @@ export class View {
     const { gates, currentGate, gameOverTimer } = state;
 
     if (gameOverTimer === -1) {
+      //Message per gate
       const gate = gates[currentGate];
       this.message.innerText = `🎵 ${gate.label}`;
     } else {
+      //Reveals the winning text at game over
       this.message.innerText = 'Gefeliciteerd! De code is ABCDEF';
     }
   }
 
   render(state) {
     this.renderMessage(state);
-
+    //Clear colour and depth buffers
     this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
     this.gl.enableVertexAttribArray(this.aPosition);
-
+    //Allocate matrices for the from
     const proj = mat4.create();
     const view = mat4.create();
     const ballModel = mat4.create();
@@ -70,16 +73,18 @@ export class View {
     const ball = state.ball.position;
 
     mat4.perspective(proj, Math.PI / 2, GAME_WIDTH / GAME_HEIGHT, 0.1, 100);
-
+    //Default camera position
     const eye = [-2.25, 1.5, -2];
+    //On game over, zoom the camera backward dramatically
     if (state.gameOverTimer > 0) {
       eye[2] -= Math.min(
         Math.pow(state.gameOverTimer, 3),
         PLATFORM_DEPTH + PLATFORM_Z
       );
     }
+    //Central looking point
     mat4.lookAt(view, eye, [-0.25, 2, 1], [0, 1, 0]);
-
+    //Worldview stays centered on the ball by shifting the world along with it
     mat4.translate(ballModel, ballModel, [0, ball[1], 0]);
 
     mat4.multiply(ballMvp, proj, view);
